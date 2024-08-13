b23f11b9ff7b676adaac62ebfd2fc11a28269e0f
Oladiran Segun Solomon <sheghunoladiran9@gmail.com>
2024-08-13 22:44:43 +0100

    implemented quick sort

--- quick-sort/main_test.go
+++ quick-sort/main_test.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"reflect"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestQuickSort(t *testing.T) {
 	expected := []int{3, 4, 7, 9, 42, 69, 420}
 	input := []int{9, 3, 7, 4, 69, 420, 42}
 	quickSort(input)
-	assert.Equal(t, input, expected, "The array's should be equal")
+	if !reflect.DeepEqual(input, expected) {
+		t.Errorf("expected: %v\n got: %v", expected, input)
+	}
 }
